2cf861c9d48c6b3f4f0b52f2a0d8655d1a4aebc1
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-07-20 19:21:30 +0200

    Some Visual effect

--- View/Home.php
+++ View/Home.php
@@ -3,6 +3,8 @@
     <p class="lead">Un développeur audacieux et motivé !</p>
     <a href="/assets/CV.pdf" class="btn btn-outline-neon mt-3" target="_blank">Téléchargez mon CV</a>
 </section>
+<hr class="my-3">
 <section class="text-center my-5">
-    <h2 class="display-4 text-neon">Le site est en cours de construction...</h2>
+    <h5 class="display-4 text-neon">Le site est en cours de construction...</h5>
 </section>
+<hr class="my-3">
