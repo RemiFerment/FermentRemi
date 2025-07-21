9bc2f2078fbe3ee3d2028d8d9805d38384835fcf
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-07-21 11:28:59 +0200

    rework home page

--- View/Home.php
+++ View/Home.php
@@ -1,4 +1,4 @@
-<section class="text-center">
+<!-- <section class="text-center">
     <h1 class="display-4 text-neon">FERMENT Rémi</h1>
     <p class="lead">Un développeur audacieux et motivé !</p>
     <a href="/assets/CV.pdf" class="btn btn-outline-neon mt-3" target="_blank">Téléchargez mon CV</a>
@@ -7,4 +7,7 @@
 <section class="text-center">
     <h3 class="text-neon">Le site est en cours de construction...</h3>
 </section>
-<hr class="my-5">
+<hr class="my-5"> -->
+<div class="container">
+    <div class="title">RÉMI FERMENT</div>
+</div>
