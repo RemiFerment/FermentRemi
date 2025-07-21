c01fc3da53caf9113ff40b06780fddb7c2f493de
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-07-21 18:37:10 +0200

    Add some link button, refact some file ans folder

--- View/Footer.php
+++ View/Footer.php
@@ -1,7 +1,10 @@
 </main>
-    <footer class="text-center mt-5 py-4 border-top border-secondary">
-        <p class="text-neon">&copy; <?php echo date('Y'); ?> FERMENT Rémi</p>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<footer class="text-center mt-5 py-4 border-top border-secondary">
+    <p class="text-neon">&copy; <?php echo date('Y'); ?> FERMENT Rémi</p>
+    <a href="https://www.linkedin.com/in/r%C3%A9mi-ferment-080803300/" class="btn btn-outline-neon mt-3" target="_blank"><i class="bi bi-linkedin"></i></a>
+    <a href="https://github.com/RemiFerment" class="btn btn-outline-neon mt-3" target="_blank"><i class="bi bi-github"></i></a>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- View/Home.php
+++ View/Home.php
@@ -1,16 +1,18 @@
-<!-- <section class="text-center">
-    <h1 class="display-4 text-neon">FERMENT Rémi</h1>
-    <p class="lead">Un développeur audacieux et motivé !</p>
-    <a href="/assets/CV.pdf" class="btn btn-outline-neon mt-3" target="_blank">Téléchargez mon CV</a>
+<!-- <h1 class="display-4 text-neon">FERMENT Rémi</h1>
+<p class="lead">Un développeur audacieux et motivé !</p>
+</section>
+<section class="text-center"> -->
+<section class="text-center">
+    <div class="container">
+        <div class="title">
+            Rémi <br> FERMENT
+        </div>
+        <div class="subtitle"> Un développeur audacieux et motivé ! </div>
+    </div>
+    <a href="/assets/CV.pdf" class="btn btn-outline-neon mt-3" target="_blank">Téléchargez mon CV <i class="bi bi-file-earmark-person"></i></a>
+    <a href="https://www.linkedin.com/in/r%C3%A9mi-ferment-080803300/" class="btn btn-outline-neon mt-3" target="_blank">Retrouvez moi sur Linkedin <i class="bi bi-linkedin"></i></a>
+    <a href="https://github.com/RemiFerment" class="btn btn-outline-neon mt-3" target="_blank">Explorez mon GitHub <i class="bi bi-github"></i></a>
 </section>
 <hr class="my-5">
-<section class="text-center">
-    <h3 class="text-neon">Le site est en cours de construction...</h3>
-</section>
-<hr class="my-5"> -->
-<div class="container">
-    <div class="title">
-        Rémi <br> FERMENT
-    </div>
-    <div class="subtitle"> Un développeur audacieux et motivé ! </div>
-</div>
+<h3 class="text-neon text-center">Le site est en cours de construction...</h3>
+<hr class="my-5">
