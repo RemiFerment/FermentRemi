5c622d30c4715e4b231c8468aa2e30f492bb62e3
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-07-21 10:50:28 +0200

    Update gitignore and fix CV access issue

--- View/Home.php
+++ View/Home.php
@@ -3,8 +3,8 @@
     <p class="lead">Un développeur audacieux et motivé !</p>
     <a href="/assets/CV.pdf" class="btn btn-outline-neon mt-3" target="_blank">Téléchargez mon CV</a>
 </section>
-<hr class="my-3">
-<section class="text-center my-5">
-    <h5 class="text-neon">Le site est en cours de construction...</h5>
+<hr class="my-5">
+<section class="text-center">
+    <h3 class="text-neon">Le site est en cours de construction...</h3>
 </section>
-<hr class="my-3">
+<hr class="my-5">
